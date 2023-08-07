e1f31488713600339bbe3c4f574684f5b213c7cb
Plouis53 <Plouis53@gmail.com>
2023-08-07 14:48:09 -0400

    added errors and revised user.js

--- models/user.js
+++ models/user.js
@@ -1,67 +1,53 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const isEmail = require("validator/lib/isEmail");
 const { UnauthorizedError } = require("../errors/unauthorized-error");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-    minlength: [2, "Name must be at least 2 characters long"],
-    maxlength: [300, "Name cannot exceed 30 characters"],
-    role: {
-      type: String,
-      default: "Clark Kent", // Set default value for role field
-    },
-  },
-  avatar: {
-    type: String,
-    required: [true, "Avatar is required"],
-    default:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Elise.png", // Set default value for avatar field
-    validate: {
-      validator: (value) => validator.isURL(value),
-      message: "You must enter a valid URL",
-    },
-  },
   email: {
     type: String,
-    required: [true, "Email is required"],
     unique: true,
+    required: true,
     validate: {
-      validator: (value) => {
-        console.log(value, "THIS IS THE EMAIL");
-        return validator.isEmail(value);
-      },
-      message: "Invalid email address",
+      validator: (v) => validator.isEmail(v),
+      message: "Incorrect email format",
     },
   },
   password: {
     type: String,
-    required: [true, "Password is required"],
-    select: false, // Add the select field to hide the password by default
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    default: "Malcolm X",
   },
 });
 
-userSchema.statics.findUserByCredentials = function findUserByCredentials(
-  email,
-  password
-) {
+userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email })
     .select("+password")
-    .then((endUser) => {
-      if (!endUser) {
-        throw new UnauthorizedError("Incorrect email or password");
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(
+          new UnauthorizedError("Incorrect password or email"),
+        );
       }
 
-      return bcrypt.compare(password, endUser.password).then((isMatch) => {
-        if (!isMatch) {
-          throw new UnauthorizedError("Incorrect email or password");
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(
+            new UnauthorizedError("Incorrect password or email"),
+          );
         }
 
-        return endUser;
+        return user;
       });
     });
 };
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("User", userSchema);
