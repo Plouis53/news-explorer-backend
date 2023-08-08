49d9ee3b0e63f6b5ec934dd88a11bb52067445b5
Plouis53 <Plouis53@gmail.com>
2023-08-08 12:10:12 -0400

    removed auth and users

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,26 @@
 const router = require("express").Router();
 const articles = require("./articles");
-const User = require("./users");
-const NotFoundError = require("../errors/not-found-error");
+const User = require("./user");
+const { NotFoundError } = require("../errors/not-found-error");
 const { signin, createUser } = require("../controllers/user");
-const auth = require("../middlewares/auth");
+// const authorization = require("../middlewares/auth");
 const {
   validateUserInfo,
   validateUserSignin,
 } = require("../middlewares/validator");
 
-router.use("/articles", auth, articles);
+router.use("/articles", articles);
 
-router.use("/users", auth, User);
+router.use("/users", User);
 
 router.post("/signin", validateUserSignin, signin);
 
 router.post("/signup", validateUserInfo, createUser);
 
 router.use(() => {
-  throw new NotFoundError("This route does not exist");
+  throw new NotFoundError(
+    "There is NO API with the requested path, or the request was sent to a non-existent address",
+  );
 });
 
 module.exports = router;
