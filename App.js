5763956a9c009858b81dd173b726bdaed4f6435d
Plouis53 <Plouis53@gmail.com>
2023-08-08 12:18:55 -0400

    added article schema

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (error) => {
+mongoose.connect("mongodb://127.0.0.1:27017/article_db", (error) => {
   if (error) {
     console.error("Error connecting to the database:", error);
   } else {
--- models/articles.js
+++ models/articles.js
@@ -1,1 +1,51 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
 
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("article", articleSchema);
