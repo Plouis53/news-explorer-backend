1fa7d549824874b4b5a1c2d95425a034b82a8de5
Plouis53 <Plouis53@gmail.com>
2023-08-15 21:58:13 -0400

    updated JWT_SECRET

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-// const { REACT_APP_JWT_SECRET = "dev-key" } = process.env;
 const { JWT_SECRET } = require("../utils/config");
 const exctractBearerToken = (header) => header.replace("Bearer ", "");
 const { UnauthorizedError } = require("../errors/unauthorized-error");
@@ -29,33 +28,3 @@ module.exports.authorization = (req, res, next) => {
     next(err);
   }
 };
-
-//8/8/23 const jwt = require("jsonwebtoken");
-// require("dotenv").config();
-
-// const { REACT_APP_JWT_SECRET = "dev-key" } = process.env;
-
-// const UnauthorizedError = require("../errors/unauthorized-error");
-
-// const exctractBearerToken = (header) => header.replace("Bearer ", "");
-
-// module.exports = (req, res, next) => {
-//   const { authorization } = req.headers;
-
-//   if (!authorization || !authorization.startsWith("Bearer ")) {
-//     return Promise.reject(new UnauthorizedError("Authorization error"));
-//   }
-
-//   const token = exctractBearerToken(authorization);
-//   let payload;
-
-//   try {
-//     payload = jwt.verify(token, REACT_APP_JWT_SECRET);
-//   } catch (err) {
-//     return Promise.reject(new UnauthorizedError("Authorization error"));
-//   }
-
-//   req.user = payload;
-
-//   return next();
-// };
