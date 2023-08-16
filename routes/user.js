01e67b6615fdf543560d837fbe6feb4ccfe000ce
Plouis53 <Plouis53@gmail.com>
2023-08-16 12:12:44 -0400

    change access

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,24 @@
 const router = require("express").Router();
 const { authorization } = require("../middlewares/auth");
 const { getCurrentUser } = require("../controllers/user");
-// const { validateUserInfo } = require("../middlewares/validator");
+const { validateUserInfo } = require("../middlewares/validator"); // Import the validation middleware
 
 router.get("/me", authorization, getCurrentUser);
 
-// router.patch("/me", authorization, validateUserInfo);
+router.patch("/me", authorization, validateUserInfo); // Use the validation middleware here
 
 module.exports = router;
+
+
+
+
+// const router = require("express").Router();
+// const { authorization } = require("../middlewares/auth");
+// const { getCurrentUser } = require("../controllers/user");
+// // const { validateUserInfo } = require("../middlewares/validator");
+
+// router.get("/me", authorization, getCurrentUser);
+
+// // router.patch("/me", authorization, validateUserInfo);
+
+// module.exports = router;
