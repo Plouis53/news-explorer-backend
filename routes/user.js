1d4ada5da1e6c854f9a453d0b9c14a496e2af3b3
Plouis53 <Plouis53@gmail.com>
2023-08-16 13:01:41 -0400

    reg updates

--- routes/user.js
+++ routes/user.js
@@ -8,17 +8,3 @@ router.get("/me", authorization, getCurrentUser);
 router.patch("/me", authorization, validateUserInfo); // Use the validation middleware here
 
 module.exports = router;
-
-
-
-
-// const router = require("express").Router();
-// const { authorization } = require("../middlewares/auth");
-// const { getCurrentUser } = require("../controllers/user");
-// // const { validateUserInfo } = require("../middlewares/validator");
-
-// router.get("/me", authorization, getCurrentUser);
-
-// // router.patch("/me", authorization, validateUserInfo);
-
-// module.exports = router;
