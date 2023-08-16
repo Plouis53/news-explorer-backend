2f1ce1bb7fd7161a76821931d15e71d0c8319703
Plouis53 <Plouis53@gmail.com>
2023-08-15 22:51:06 -0400

    comment out patch

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const { authorization } = require("../middlewares/auth");
 const { getCurrentUser } = require("../controllers/user");
-const { validateUserInfo } = require("../middlewares/validator");
+// const { validateUserInfo } = require("../middlewares/validator");
 
 router.get("/me", authorization, getCurrentUser);
 
-router.patch("/me", authorization, validateUserInfo);
+// router.patch("/me", authorization, validateUserInfo);
 
 module.exports = router;
