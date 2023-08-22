bae92720fd7944dd89bd9d79d75c9e786b3221f5
Plouis53 <Plouis53@gmail.com>
2023-08-21 20:19:05 -0400

    started

--- app.js
+++ app.js
@@ -16,25 +16,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/article_db", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb://127.0.0.1:27017/article_db")
   .then((res) => {
     console.log("Database connected");
   })
   .catch((error) => {
     console.log(error);
   });
-
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/article_db")
-//   .then(() => {
-//     console.log("Connected to the database");
-//   })
-//   .catch((error) => {
-//     console.error("Error connecting to the database:", error);
-//   });
 
 const routes = require("./routes");
 
@@ -60,77 +48,5 @@ app.use(errorHandler); // our centralized handler
 app.listen(PORT, () => {
   console.log("hello");
   console.log(`App listening at port ${PORT}`);
-  console.log("Working");
+  console.log("Connected to the database");
 });
-
-// require("dotenv").config();
-// const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const { errors } = require("celebrate"); // Import celebrate before error-handler
-// const helmet = require("helmet");
-// const bodyParser = require("body-parser");
-
-// const errorHandler = require("./middlewares/error-handler");
-// const { requestLogger, errorLogger } = require("./middlewares/logger");
-
-// const { PORT = 3001 } = process.env;
-// const app = express();
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/article_db", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then((res) => {
-//     console.log("Database connected");
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-// // mongoose.connect("mongodb://127.0.0.1:27017/article_db", (error) => {
-// //   if (error) {
-// //     console.error("Error connecting to the database:", error);
-// //   } else {
-// //     console.log("Connected to the database");
-// //   }
-// // });
-
-// mongoose.connect("mongodb://127.0.0.1:27017/article_db", (error) => {
-//   if (error) {
-//     console.error("Error connecting to the database:", error);
-//   } else {
-//     console.log("Connected to the database");
-//   }
-// });
-
-// const routes = require("./routes");
-
-// app.use(express.json());
-// app.use(cors());
-
-// app.use(helmet());
-
-// app.use(requestLogger);
-
-// app.get("/crash-test", () => {
-//   setTimeout(() => {
-//     throw new Error("Server will crash now");
-//   }, 0);
-// });
-
-// app.use(routes); // our routes
-// app.use(errorLogger); // enabling the error logger
-
-// app.use(errors()); // celebrate error handler should be used before error-handler
-// app.use(errorHandler); // our centralized handler
-
-// app.listen(PORT, () => {
-//   console.log("hello");
-//   console.log(`App listening at port ${PORT}`);
-//   console.log("Working");
-// });
