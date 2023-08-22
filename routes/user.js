7679fcf65fff50b7c6b6f6de6683ade28fe86c77
Plouis53 <Plouis53@gmail.com>
2023-08-22 15:15:55 -0400

    Merge stage-back-end into main

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { authorization } = require("../middlewares/auth");
 const { getCurrentUser } = require("../controllers/user");
-const { validateUserInfo } = require("../middlewares/validator"); // Import the validation middleware
+const { validateUserInfo } = require("../middlewares/validator"); 
 
 router.get("/me", authorization, getCurrentUser);
 
